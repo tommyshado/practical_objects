e5bec538653cf534d92f6ec2224e53b5c51c9bb6
Mthunzi Sidwell Tom <107440311+tommyshado@users.noreply.github.com>
2023-04-15 19:20:01 +0200

    Delete codewars directory

--- codewars/regex_validate_pin_code.py
+++
@@ -1,9 +0,0 @@
-# ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
-
-# If the function is passed a valid PIN string, return true, else return false.
-
-def validate_pin(pin):
-    # parentesis 
-    if pin.isdigit() and (len(pin) == 4 or len(pin) == 6):
-        return True
-    return False
